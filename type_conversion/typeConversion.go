496012966071a73896ca8cde2813e5b4959da8b9
jiang7552261 <jiang7552261@163.com>
2023-05-29 14:28:27 +0800

    添加公共库方法

---
+++ type_conversion/typeConversion.go
@@ -0,0 +1,13 @@
+package type_conversion
+
+import "strconv"
+
+//------------- float -------------
+
+func Float64ToInt64(in float64) int64 {
+	return int64(in)
+}
+
+func Float64ToString(in float64, prec int) string {
+	return strconv.FormatFloat(in, 'f', prec, 64)
+}
