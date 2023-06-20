c749e0ac3297bf1dac28720135945f1fb4f9c909
jiang7552261 <jiang7552261@163.com>
2023-06-20 15:32:54 +0800

    调整

--- tool/typeConversion.go
+++ tool/typeConversion.go
@@ -11,3 +11,8 @@ func Float64ToInt64(in float64) int64 {
 func Float64ToString(in float64, prec int) string {
 	return strconv.FormatFloat(in, 'f', prec, 64)
 }
+
+func StringToFloat64(in string) float64 {
+	out, _ := strconv.ParseFloat(in, 64)
+	return out
+}
